a527a1d60046536ded5b2cea6bf791dfd1db25cb
fstipo <fstipanov@gmail.com>
2022-03-25 15:27:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import Banner from './Components/Banner/Banner';
+import Form from './Components/Form/Form';
+import Footer from './Components/Footer/Footer';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Banner />
+      <Form />
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Footer = () => {
+  return (
+    <footer className="d-block">
+      <p className="text-center"> ©Copyright 2022 Teška krađa </p>
+    </footer>
+  );
+};
+export default Footer;
